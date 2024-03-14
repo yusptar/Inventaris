74710eb40e6f66d0189df5e4fb9818c491d37da7
yusptar <yusupvatar@gmail.com>
2024-03-14 14:28:39 +0700

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+"use client";
+
+export * from "./navbar";
+export * from "./footer";
+export * from "./layout";
+export * from "./stats-card";
+export * from "./feedback-card";
+export * from "./category-card";
+export * from "./course-card";
+export * from "./event-card";
+export * from "./footer";
+export * from "./fixed-plugin";
