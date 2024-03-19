e4b1da68cd029f827509276967923772d18c53d9
yusptar <yusupvatar@gmail.com>
2024-03-19 13:07:37 +0700

    deleted useless page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,7 @@
 
-export * from "./out-impressive-stats";
 export * from "./hero";
 export * from "./layout";
 export * from "./page";
-export * from "./students-feedback";
 export * from "./list-resep";
-export * from "./testimonial";
-export * from "./courses-categories";
-export * from "./events";
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,15 @@
 // components
 import { Navbar, Footer } from "@/components";
-
 // sections
 import Hero from "./hero";
-import OutImpressiveStats from "./out-impressive-stats";
-import CoursesCategories from "./courses-categories";
 import ListResep from "./list-resep";
-import Testimonial from "./testimonial";
-import Events from "./events";
-import StudentsFeedback from "./students-feedback";
-import TrustedCompany from "./trusted-companies";
 
 export default function Campaign() {
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      {/* <OutImpressiveStats /> */}
-      {/* <CoursesCategories /> */}
       <ListResep />
-      {/* <Testimonial /> */}
-      {/* <Events /> */}
-      {/* <StudentsFeedback /> */}
-      {/* <TrustedCompany /> */}
       <Footer />
     </>
   );
