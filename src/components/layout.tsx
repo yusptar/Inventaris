edf89ec2ecdca29c09e97d0897a1542630907dca
yusptar <yusupvatar@gmail.com>
2024-03-25 12:35:35 +0700

    fixing layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,8 @@
 import React, { ReactNode } from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export function Layout({ children }: { children?: ReactNode }) {
-  return <ThemeProvider>{children ? children : null}</ThemeProvider>;
+export function Layout({ children }: { children: ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
 
 export default Layout;
