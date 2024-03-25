05625b17332a64398cc6c85d812254ad53a51757
yusptar <yusupvatar@gmail.com>
2024-03-25 12:07:29 +0700

    coba

--- src/app/list-resep.tsx
+++ src/app/list-resep.tsx
@@ -60,7 +60,7 @@ const ListResep = () => {
   return (
     <section className="px-8 mt-12">
       <div className="container mx-auto mb-24 text-center">
-        <Typography variant="h2" color="blue-gray">
+        <Typography placeholder onPointerEnterCapture onPointerLeaveCapture variant="h2" color="blue-gray">
           List Resep Obat Pasien
         </Typography>
         {/* <Typography
